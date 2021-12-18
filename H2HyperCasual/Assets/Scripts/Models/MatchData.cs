e83ee38e46ff887e6b1146e6cd8d9feb448741c4
Michael Schmidt Nissen <michaelschmidtnissen@gmail.com>
2021-12-18 12:36:40 +0100

    Added MatchData and MatchManager classes

---
+++ H2HyperCasual/Assets/Scripts/Models/MatchData.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchData : MonoBehaviour
+{
+    public int MatchStatus { get; set; }
+    public int WinnerPlayerId { get; set; }
+    public int PlayTime { get; set; }
+    public List<Player> Players { get; set; }
+    public List<MatchKill> MatchKills { get; set; }
+
+    public bool AddPlayer(Player player)
+    {
+        if(!Players.Contains(player))
+        {
+            Players.Add(player);
+            return true;
+        }
+
+        return false;
+    }
+
+    public bool AddMatchKill(MatchKill matchKill)
+    {
+        if (!MatchKills.Contains(matchKill))
+        {
+            MatchKills.Add(matchKill);
+            return true;
+        }
+
+        return false;
+    }
+}
