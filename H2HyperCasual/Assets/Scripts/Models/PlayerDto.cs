9e25fd7794957e5ee42cd7a832f5292aeb90b338
Michael Schmidt Nissen <michaelschmidtnissen@gmail.com>
2021-12-21 18:21:40 +0100

    PlayerDto

---
+++ H2HyperCasual/Assets/Scripts/Models/PlayerDto.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDto : MonoBehaviour
+{
+    public int playerId;
+    public int matchId;
+    public int playerMatchKills;
+    public int playerMatchDeaths;
+}
